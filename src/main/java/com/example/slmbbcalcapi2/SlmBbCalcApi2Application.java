3244b14344a2cdfde095c84ab63f642fb110ba6a
zorenyikristof <zorenyi.kristof@gmail.com>
2023-06-06 18:45:49 +0200

    initial commit

---
+++ src/main/java/com/example/slmbbcalcapi2/SlmBbCalcApi2Application.java
@@ -0,0 +1,13 @@
+package com.example.slmbbcalcapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmBbCalcApi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmBbCalcApi2Application.class, args);
+    }
+
+}
